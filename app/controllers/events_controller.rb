e59f23bc74a5121ec5434eb9277baaae3a297b32
MarionKnight <knight499@gmail.com>
2013-11-30 15:40:16 -0800

    finished delete btn

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,9 +16,15 @@ class EventsController < ApplicationController
   def create
     @host = Host.find(params[:host_id])
     @event = Event.create(params[:event])
+    @host.events << @event
     redirect_to host_event_path(@host, @event)
   end
 
   def destroy
+    @host = Host.find(params[:host_id])
+    @event = Event.find(params[:id])
+    @specific_event = @host.events.find(params[:id])
+    @specific_event.destroy
+    redirect_to host_events_path(@host)
   end
 end
