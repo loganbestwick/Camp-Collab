3b98eceead59bba1932cc05e1e5aee93bb65e416
MarionKnight <knight499@gmail.com>
2013-11-30 14:30:13 -0800

    finished crud

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,18 +3,20 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
     @event = Event.new
+    @host = Host.find(params[:host_id])
   end
 
   def show
     @item = Item.new
-    @host  = Host.find(params[:id])
+    @host  = Host.find(params[:host_id])
     @event = Event.find(params[:id])
     @items = @event.items
   end
 
   def create
+    @host = Host.find(params[:host_id])
     @event = Event.create(params[:event])
-    redirect_to host_event_path(@event)
+    redirect_to host_event_path(@host, @event)
   end
 
   def destroy
