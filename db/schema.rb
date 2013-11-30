bb680e9a03ec8e42cfc121c0e95e0f92eb3b2b98
Apprentice <apprentice@dbc-23.local>
2013-11-30 14:29:23 -0800

    host can log in, log out, and sign up.

--- db/schema.rb
+++ db/schema.rb
@@ -39,11 +39,11 @@ ActiveRecord::Schema.define(:version => 20131130012854) do
   create_table "hosts", :force => true do |t|
     t.string   "name"
     t.string   "username"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "email"
     t.string   "phone"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   create_table "items", :force => true do |t|
