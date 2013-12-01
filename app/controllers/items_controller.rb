ccbb7d61c809fa63161ef06d9e67871f2bcd5ae7
MarionKnight <knight499@gmail.com>
2013-11-30 17:04:56 -0800

    can add items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,11 +4,14 @@ class ItemsController < ApplicationController
   end
 
   def create
-    p '*' * 60
-    p params
+    @event = Event.find(params[:event_id])
+    @item = Item.create(name: params[:item][:name], price: params[:item][:price], event_id: params[:event_id])
+
+    redirect_to host_event_path(session[:host_id], @event)
   end
 
   def update
+
   end
 
   def destroy
