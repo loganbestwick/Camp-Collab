93121378542a9fb8d98151c562b584216bc35538
Apprentice <apprentice@dbc-23.local>
2013-11-30 21:15:19 -0800

    Event is marked as completed if all of the items in the event are marked as purchased.

--- db/migrate/20131130011837_create_event_table.rb
+++ db/migrate/20131130011837_create_event_table.rb
@@ -3,7 +3,7 @@ class CreateEventTable < ActiveRecord::Migration
     create_table :events do |t|
       t.string :name, :address, :city, :state, :description
       t.integer :expected_attendance, :zip
-
+      t.boolean :completed, :null => false, :default => false
       t.timestamps
       t.belongs_to :host
     end
