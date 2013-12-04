e4de224897362a1528c8e4c84987ed17d8b69777
MarionKnight <knight499@gmail.com>
2013-12-03 16:18:14 -0800

    mandrill emails working
    We also used our newfound git ability to take away those goddamn fucking coverage emails.  Fuck you, Simplecov, whore of babylon, we don't need your 10,000 lines of Json fuckery.

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -5,6 +5,7 @@ class GuestsController < ApplicationController
     @token = SecureRandom.urlsafe_base64
     @guest = Guest.new(name: params[:guest][:name], email: params[:guest][:email], event_id: @event.id, token: @token)
     @guest.save
+    GuestMailer.guest_invitation.deliver
     redirect_to host_event_path(session[:host_id], @event)
   end
 
