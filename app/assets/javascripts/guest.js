4b0ec83f2f3fd918f495f325b06ae63a716c4e7b
Logan Bestwick <loganbestwick@gmail.com>
2013-12-05 10:22:21 -0800

    cleaned js file

--- app/assets/javascripts/guest.js
+++ app/assets/javascripts/guest.js
@@ -24,15 +24,3 @@ $(document).ready(function(){
   Event.init()
 
 })
-
-
-  <script type="text/javascript">
-  handler = Gmaps.build('Google');
-  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
-    markers = handler.addMarkers(<%=raw @hash.to_json %>);
-    handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
-    handler.getMap().setZoom(12);
-  });
-
-  </script>
